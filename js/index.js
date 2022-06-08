79fa8c3dd7908b2ddff62d730ac556d497d41feb
la1001 <103139706+la1001@users.noreply.github.com>
2022-06-07 22:25:20 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+console.dir (" Hola!")
+
+
+var pepes={
+
+    nombre:"jose",
+    apellido: "gomes",
+    edad:25,
+    dni:"30.487.788",
+
+    cumpliranios: function(params) {
+        
+    }
+
+};
